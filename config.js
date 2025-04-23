103c4ae1d860209b36402f7a636ff4e787b68d15
znazna00 <mendorong@gmail.com>
2025-04-23 11:32:50 +0900

    프로필 변경

--- config.js
+++ config.js
@@ -13,10 +13,10 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "licat",
-    company: "weniv",
-    position: "CEO",
-    img: "img/user/profile-licat.png",
+    username: "dorong",
+    company: "thedot",
+    position: "manager",
+    img: "img/user/profile-dorong.png",
   },
 ];
 
